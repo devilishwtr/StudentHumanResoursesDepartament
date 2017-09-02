c19d4f646f985edf13d0b400630851270514efc3
devilishwtr <odanowhome@gmail.com>
2017-09-02 17:35:59 +0700

    standard idea files

--- src/ru/osipov/shrd/Main.java
+++ src/ru/osipov/shrd/Main.java
@@ -1,8 +1,25 @@
 package ru.osipov.shrd;
 
+import java.util.Scanner;
+
 public class Main{
+    static Scanner in = new Scanner (System.in);
+    static int welcomeAge = in.nextInt ();
+    static String welcomeChoise = in.next ();
 
     public static void main(String[] args) {
-	// write your code here
+        hello(welcomeAge,welcomeChoise);
+    }
+
+    public static void hello (int num, String choise){
+        welcomeAnswer ();
+        if (num>18 && choise.equals ("yes")) {
+            System.out.println ("Hello Boy. Welcome to adult world.");
+        }
+    }
+
+    private static void welcomeAnswer () {
+        System.out.println ("Before you come to us, we need to ask if you are 18 years old and are you sure of this?\n" +
+                "Enter your age and write in a space word yes");
     }
 }
